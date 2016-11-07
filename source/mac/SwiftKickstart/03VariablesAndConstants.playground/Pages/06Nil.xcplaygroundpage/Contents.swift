301db2431c1bd8e079358e345059ed5c1f25d334
Daniel Steinberg <daniel@dimsumthinking.com>
2016-11-07 12:46:10 -0500

    3-6 drafted

--- source/mac/SwiftKickstart/03VariablesAndConstants.playground/Pages/06Nil.xcplaygroundpage/Contents.swift
+++ source/mac/SwiftKickstart/03VariablesAndConstants.playground/Pages/06Nil.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
-//: ### Return Values
+//: ### Nil
 //: [TOC](TOC) | [Previous](@previous) | [Next](@next)
-func hello() -> String {
-    return "Hello, World!"
+var name: String?
+name = "Swifty"
+print(name ?? "name is nil")
+name = nil
+print(name ?? "name is nil")
+
+import UIKit
+let image = UIImage(named: "Kickstart.jpg")
+
+let imageView = UIImageView()
+//if image != nil {
+//    let validImage = image!
+//    imageView.image = validImage
+//} else {
+//    print("There is no image to display")
+//}
+//imageView.image
+
+//if let validImage = image {
+//    imageView.image = validImage
+//} else {
+//    print("There is no image to display")
+//}
+//imageView.image
+
+if let image = image {
+    imageView.image = image
+} else {
+    print("There is no image to display")
 }
-hello()
+imageView.image
+
 //: [TOC](TOC) | [Previous](@previous) | [Next](@next)
