49a059b02756c0402376536110786d765032916a
Daniel Steinberg <daniel@dimsumthinking.com>
2016-11-08 22:44:42 -0500

    chapter 3

--- source/mac/SwiftKickstart/03VariablesAndConstants.playground/Pages/10IfLet.xcplaygroundpage/Contents.swift
+++ source/mac/SwiftKickstart/03VariablesAndConstants.playground/Pages/10IfLet.xcplaygroundpage/Contents.swift
@@ -1,35 +1,30 @@
 //: ### if let
 //: [TOC](TOC) | [Previous](@previous) | [Next](@next)
-var name: String?
-name = "Swifty"
-print(name ?? "name is nil")
-name = nil
-print(name ?? "name is nil")
-
 import UIKit
+let viewer = UIImageView()
 let image = UIImage(named: "Kickstart.jpg")
 
-let imageView = UIImageView()
 //if image != nil {
 //    let validImage = image!
-//    imageView.image = validImage
+//    viewer.image = validImage
 //} else {
 //    print("There is no image to display")
 //}
-//imageView.image
+//viewer.image
 
 //if let validImage = image {
-//    imageView.image = validImage
+//    viewer.image = validImage
 //} else {
 //    print("There is no image to display")
 //}
-//imageView.image
+//viewer.image
 
 if let image = image {
-    imageView.image = image
+    viewer.image = image
 } else {
     print("There is no image to display")
 }
-imageView.image
+viewer.image
+
 
 //: [TOC](TOC) | [Previous](@previous) | [Next](@next)
