eb2c07fa8b3d750ba6ed89c452542707e85ac933
Daniel Steinberg <daniel@dimsumthinking.com>
2016-11-12 08:52:22 -0500

    4-3 drafted

---
+++ source/mac/SwiftKickstart/04Collections.playground/Pages/02CreateModifiableArrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: ### Create Modifiable Arrays
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
+
+var drinkSizes = ["Big", "Really Big", "Huge"]
+var sales = Array(repeating: 0, count: 3)
+
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
--- source/mac/SwiftKickstart/04Collections.playground/Pages/03Var.xcplaygroundpage/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: ### Var
-//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
-
-var prime = 5
-prime = 7
-
-//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
