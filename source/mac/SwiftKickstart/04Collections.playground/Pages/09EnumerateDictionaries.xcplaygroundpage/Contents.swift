311384f3b724e60dec1a2eca91fceebc10edaa22
Daniel Steinberg <daniel@dimsumthinking.com>
2016-11-13 21:40:46 -0500

    4-9 staged

--- source/mac/SwiftKickstart/04Collections.playground/Pages/09EnumerateDictionaries.xcplaygroundpage/Contents.swift
+++ source/mac/SwiftKickstart/04Collections.playground/Pages/09EnumerateDictionaries.xcplaygroundpage/Contents.swift
@@ -4,19 +4,19 @@
 let numbers = ["one":1, "two":2, "three":3]
 
 let numbersKeys = numbers.keys
-let numbersValues = numbers.values
 
 for key in numbersKeys {
     print(key, ":", numbers[key]!)
 }
+
+let numbersValues = numbers.values
 
 for value in numbersValues {
     print(value)
 }
 
 for number in numbers {
-    print(number)
-    number
+    print(number.0, ":", number.1)
 }
 
 for (key, value) in numbers {
