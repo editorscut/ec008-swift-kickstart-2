988b2e42bd91acf0b170169cff6031c1ac8f88f9
Daniel Steinberg <daniel@dimsumthinking.com>
2016-11-17 14:14:49 -0500

    5-6 staged

---
+++ source/mac/SwiftKickstart/05Structs.playground/Pages/06Equatable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: ### Equatable
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
+
+struct Vertex {
+    let x, y: Double
+}
+
+extension Vertex: CustomStringConvertible {
+    var description: String {
+        return "(\(x), \(y))"
+    }
+}
+
+extension Vertex: Equatable{}
+
+func ==(lhs: Vertex, rhs: Vertex) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
+let pointThreeFour = Vertex(x: 3, y: 4)
+let pointTwoFive = Vertex(x: 2, y: 5)
+let pointThreeFive = Vertex(x: 3, y: 5)
+let pointTwoFour = Vertex(x: 2, y: 4)
+let anotherPointThreeFour = Vertex(x: 3, y: 4)
+
+// completely different
+pointThreeFour == pointTwoFive
+// agree in x or y not both
+pointThreeFour == pointThreeFive
+pointThreeFour == pointTwoFour
+// same value
+pointThreeFour == anotherPointThreeFour
+
+pointThreeFour != pointTwoFive
+pointThreeFour != anotherPointThreeFour
+
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
