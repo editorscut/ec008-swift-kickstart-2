15579d36de4103bfa9dee244701af3398c0df138
Daniel Steinberg <daniel@dimsumthinking.com>
2016-11-17 11:57:24 -0500

    5-5 staged

---
+++ source/mac/SwiftKickstart/05Structs.playground/Pages/05CustomStringConvertible.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: ### CustomStringConvertible
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
+struct Vertex {
+    let x, y: Double
+}
+
+extension Vertex: CustomStringConvertible {
+    var description: String {
+        return "(\(x), \(y))"
+    }
+}
+
+let point = Vertex(x: 3, y: 4)
+
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
