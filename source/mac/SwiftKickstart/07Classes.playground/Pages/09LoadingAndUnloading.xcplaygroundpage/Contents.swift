f679cf2277c902ad6ae6805f21d557c79896be73
Daniel Steinberg <daniel@dimsumthinking.com>
2016-12-01 18:56:59 -0500

    7-9 drafted

--- source/mac/SwiftKickstart/07Classes.playground/Pages/09LoadingAndUnloading.xcplaygroundpage/Contents.swift
+++ source/mac/SwiftKickstart/07Classes.playground/Pages/09LoadingAndUnloading.xcplaygroundpage/Contents.swift
@@ -1,4 +1,45 @@
 //: ### Loading and Unloading
 //: [TOC](TOC) | [Previous](@previous) | [Next](@next)
 
+var log = ""
+var count = 1
+
+func updateLog(with status: String) {
+    log += "(\(count)) " + status + "\n"
+    count += 1
+}
+
+class Location {
+    let city: String
+    
+    init(city: String) {
+        self.city = city
+        updateLog(with: "Create Location")
+    }
+    deinit {
+        updateLog(with: "Destroy Location")
+    }
+}
+
+class Attendee {
+    let name: String
+    lazy var location = Location(city: "San Francisco")
+    
+    init(name: String) {
+        self.name = name
+        updateLog(with: "Create Attendee")
+    }
+    deinit {
+        updateLog(with: "Destroy Attendee")
+    }
+}
+
+var daniel = Attendee(name: "Daniel")
+log
+daniel.location.city
+log
+daniel = Attendee(name: "Kimberli")
+daniel.location.city
+log
+
 //: [TOC](TOC) | [Previous](@previous) | [Next](@next)
--- source/mac/SwiftKickstart/07Classes.playground/Pages/TOC.xcplaygroundpage/Contents.swift
+++ source/mac/SwiftKickstart/07Classes.playground/Pages/TOC.xcplaygroundpage/Contents.swift
@@ -1,11 +1,13 @@
 /*:
  # Table of Contents
- * [01 Create Structs](01CreateStructs)
- * [02 Stored Properties](02StoredProperties)
- * [03 Computed Properties](03ComputedProperties)
- * [04 willSet() didSet()](04WillSetDidSet)
- * [05 CustomStringConvertible](05CustomStringConvertible)
- * [06 Equatable](06Equatable)
- * [07 Value Types](07ValueTypes)
- * [08 Methods](08Methods)
+ * [01 Create Classes](01CreateClasses)
+ * [02 Reference Types](02ReferenceTypes)
+ * [03 Initializers](03Initializers)
+ * [04 Subclasses](04Subclasses)
+ * [05 Methods](05Methods)
+ * [06 Optional Properties](06OptionalProperties)
+ * [07 Failable Initializers](07FailableInitializers)
+ * [08 Optional Chaining](08OptionalChaining)
+ * [09 Loading and Unloading](09LoadingAndUnloading)
+ * [10 Composition](10Composition)
  */
