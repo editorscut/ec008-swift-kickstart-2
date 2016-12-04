ceaa24cddb2ffcbc47e0a8cb6d6cf34d8e0239b6
Daniel Steinberg <daniel@dimsumthinking.com>
2016-12-04 13:02:45 -0500

    8-9 drafted

---
+++ source/mac/SwiftKickstart/08Protocols.playground/Pages/09AccessLevels.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: ### Access Levels
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
+
+let vertex = Vertex(x: 3, y: 4)
+let rectangle = Rectangle(topLeftCorner: vertex, width: 100, height: 50)
+
+let movedVertex = vertex.movedHorizontally(by: 10)
+let movedTwiceVertex = movedVertex.movedHorizontally(by: 10)
+
+vertex.location
+rectangle.location
+
+let shiftedLeftVertex = shiftedLeft(movable: vertex)
+let shiftedLeftRectangle = shiftedLeft(movable: rectangle)
+
+let shiftedRightVertex = vertex.shiftedRight()
+let shiftedRightRectangle = rectangle.shiftedRight()
+
+//: [TOC](TOC) | [Previous](@previous) | [Next](@next)
